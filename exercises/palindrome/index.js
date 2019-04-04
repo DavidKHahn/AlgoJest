34fd30e28a75766cb35d438d3b9f6dbeacc5ef9b
David <dhahn123@gmail.com>
2019-04-04 10:27:11 -0700

    Finished Palindrome Exercise

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,26 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// Solution #1
+// function palindrome(str) {
+//     const reversed = str
+//         .split('')
+//         .reverse()
+//         .join('');
+
+//     return str === reversed;
+// }
+
+// Solution #2
+function palindrome(str) {
+    // convert str into array and use 'every()' to run through each item 
+    // 'char' represents each character received from array, 'i' represents array elements used for iteration
+    return str.split('').every((char, i) => {
+        // compares each value with opposite side of array: [str.length - i - 1] <--checks from reversed order
+        return char === str[str.length - i - 1]
+    })
+}
+
+
 
 module.exports = palindrome;
