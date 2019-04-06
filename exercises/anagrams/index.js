fb9288a0d3d0c2c27141810fa4b0ea43c4667b0c
David <dhahn123@gmail.com>
2019-04-05 19:39:31 -0700

    Finished Anagrams && Capitalize First Letter Algorithms

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// Solution #1:
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMap(stringA);
+//     const bCharMap = buildCharMap(stringB);
+// // Object.keys -> returns keys inside object and adding '.length' counts the total # of keys inside
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//         return false;
+//     }
+// // for.. in checks objects
+//     for (let char in aCharMap) {
+//         if (aCharMap[char] !== bCharMap[char]){
+//             return false;
+//         }
+//     }
+// // if both cases succeed for this problem: return true
+//     return true;
+// }
+
+
+// function buildCharMap(str) {
+//     const charMap = {};
+//     // runs a loop through each character string using regex + lowercase method and checks if char exists if not includes into object
+//     for(let char of str.replace(/[^w]/g, '').toLowerCase()){
+//         charMap[char] = charMap[char] + 1 || 1;
+//     }
+//     return charMap;
+// }
+
+// Solution #2:
+function anagrams(stringA, stringB){
+    // checks if both strings are anagrams
+    return cleanStr(stringA) === cleanStr(stringB);
+}
+
+// helper function
+// sort() -> array method which set characters in alphabetical or numerical order based on JS runtime environment
+// use 'split('')' to turn into array first and then '.sort()' then 'join('')' to return back into string
+function cleanStr(str){
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
 
 module.exports = anagrams;
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,38 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// Solution #1:
+// function capitalize(str) {
+//     // set up empty array
+//     let words = [];
+//     // run for of loop while using split(' ') returning an array of spaced out words
+//     for (let word of str.split(' ')) {
+//     // grab each word starting with 1st char and capitalize first letter
+//     // use slice() method to grab each set of words' characters after the 1st letter then combine the capitalized 1st letter
+//     // push word back into words array
+//          words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+//     // returns each word back into a string format
+//     return words.join(' ');
+// }
+
+// Solution #2:
+function capitalize(str){
+    // automatically make 1st char uppercase
+    let result = str[0].toUpperCase();
+    // for loop excluding 1st char
+    for (let i = 1; i < str.length; i++){
+        // if str char to left equals empty space then capitalize current str char and add to result
+        if(str[i - 1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+        // else if current str does not have empty space to left then add to result as is
+            result += str[i];
+        }
+    }
+    // return string
+    return result;
+}
+
 
 module.exports = capitalize;
